fd45500b70a909bfb39461a7a909660e978a8ed0
BEMKK <busnyakerik@gmail.com>
2026-09-08 14:20:47 +0200

    Újdonságok és verziószám javítása

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 APP_NAME = "KönyvTárnok"
-APP_VERSION = "0.22.1"
+APP_VERSION = "0.22.2"
 APP_STAGE = "Béta"
 APP_TITLE = f"{APP_NAME} (v{APP_VERSION}) {APP_STAGE}"
--- dialogs.py
+++ dialogs.py
@@ -1052,7 +1052,6 @@ class UjdonsagokDialog(wx.Dialog):
         main_sizer.Add(wx.StaticLine(self), 0, wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, 15)
         
         ujdonsagok_lista = [
-            "Javítva a right click menü billentyűzetes előhívásának hibája.",
             "Bővítve a súgó tartalma és a billentyűparancsok listája.",
             "A Dezideráta-kezelőben a Ctrl+R már nem nyitja meg a tétel részleteit, azok megtekintésére kizárólag az enter billentyű vagy a dupla kattintás szolgál."
         ]
